f5c41dc1738f4c6770835415bb61efd4c87224a3
dflail <dan.flail.dev@gmail.com>
2026-08-10 09:56:40 -0400

    File execution

--- functions/write_file.py
+++ functions/write_file.py
@@ -6,19 +6,19 @@ import os
 def write_file(working_directory: str, file_path: str, content: str) -> str:
     try:
         abs_work_dir = os.path.abspath(working_directory)
-        target_file_path = os.path.normpath(os.path.join(abs_work_dir, file_path))
+        abs_file_path = os.path.normpath(os.path.join(abs_work_dir, file_path))
 
-        if not os.path.commonpath([abs_work_dir, target_file_path]) == abs_work_dir:
+        if not os.path.commonpath([abs_work_dir, abs_file_path]) == abs_work_dir:
             return f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory'
 
         # Ensure the directory exists
-        print(f"DEBUGGING: {os.path.dirname(target_file_path)}")
-        os.makedirs(os.path.dirname(target_file_path), exist_ok=True)
+        print(f"DEBUGGING: {os.path.dirname(abs_file_path)}")
+        os.makedirs(os.path.dirname(abs_file_path), exist_ok=True)
 
-        with open(target_file_path, 'w') as file:
+        with open(abs_file_path, 'w') as file:
             file.write(content)
         
-        if os.path.isdir(target_file_path):
+        if os.path.isdir(abs_file_path):
             return f'Error: Cannot write to "{file_path}" as it is a directory'
         
         return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
